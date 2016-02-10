8d7216378df98fb0ef83484e4c029be2fb836a0e
nic netzel and torrence stratton <tlstratton@gmail.com>
2016-02-10 11:29:30 -0800

    add display pages

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Places.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_places'])){
+        $_SESSION['list_of_places'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array( 'twig.path'=>__DIR__."/../views"
+    ));
+
+    $app->get('/', function() use ($app){
+        return $app['twig']->render('index.html.twig', array('places'=> Place::getAll()));
+    });
+
+    $app->post('/place_list', function() use ($app) {
+        $place = new Place($_POST['placeInput']);
+        $place->savePlace();
+        return $app['twig']->render('place_list.html.twig', array('newplace' => $place));
+    });
+
+    return $app;
+?>
---
+++ src/Places.php
@@ -0,0 +1,28 @@
+<?php
+class Place {
+    private $city;
+
+    function __construct($city)
+    {
+        $this->city = $city;
+    }
+
+    function setCity($newCity) {
+        $this->city = (string) $newCity;
+    }
+
+    function getCity() {
+        return $this->city;
+    }
+
+    function savePlace() {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+}
+ ?>
